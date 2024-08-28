b498f212fa43ab3d07fea879ade3c2ab19e76cb0
Misteri02 <julius.tuominen02@gmail.com>
2024-08-28 10:42:35 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
-
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
       <p>
-        {a} plus {b} is {a + b}
+        Hello {props.name}, you are {props.age} years old
       </p>
     </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by 
+      <a href="https://github.com/misteri02">misteri02</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Pekka'
+  const age = 10
+  const friends = [
+    { name: 'Leevi', age: 4 },
+    { name: 'Venla', age: 10 },
+  ]
+
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </>
   )
 }
 export default App
